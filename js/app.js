69e418c37e26437d9ab8046cda38282e8f6abc34
Rifai <abdelrahman.rifai@gmail.com>
2017-08-21 18:10:57 +0200

    Get data from JSONPlaceholder via Ajax

--- js/app.js
+++ js/app.js
@@ -4,6 +4,27 @@ $(function(){
     var self = this;
     self.posts = ko.observableArray();
 
+    $.ajax({
+      url: 'https://jsonplaceholder.typicode.com/posts',
+      method: 'GET'
+    }).then(function(data) {
+      // Lmit data to 10 posts
+      $.each(data.slice(0, 10), function () {
+        self.posts.push({
+          title: this.title,
+          content: this.body
+        });
+      });
+      // Carousel initialization
+      $('.carousel').carousel({
+        indicators: true,
+        shift: 20,
+        padding: 20
+      });
+    }, function(e) { // Fire a toast message in case Ajax failed
+      Materialize.toast('Failed to load posts, try again later!', 8000);
+    });
+
   };
 
 });
