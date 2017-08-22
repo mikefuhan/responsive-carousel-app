147f4aaeb2de72cc5457f99f4c62f98b70b23f7a
Rifai <abdelrahman.rifai@gmail.com>
2017-08-22 11:30:41 +0200

    Rephrase some comments

--- js/app.js
+++ js/app.js
@@ -11,18 +11,18 @@ $(function(){
         method: 'GET'
       }),
       $.ajax({
-        url: self.endpoint + '/albums/1/photos', // Get photos from one album instead of loading 5000 photos!
+        url: self.endpoint + '/albums/1/photos', // Get photos from one album instead of loading 5000 items!
         method: 'GET'
       })
     ).then(function(posts, photos) {
-      posts = posts[0].slice(0, 10); // Lmit data to 10 posts
-      photos = photos[0].slice(0, 10); // Get first 10 photos objects
+      posts = posts[0].slice(0, 10); // Lmit data to 10 items
+      photos = photos[0].slice(0, 10); // Get first 10 items
       $.each(posts, function(i) {
         self.cardsData.push({
           title: this.title,
           content: this.body,
           // Update URL to the new HTTPS placeholder.com website formerly placehold.it
-          // Change photos dimensions for faster loading & Remove photos dimensions text
+          // Change photos dimensions and remove dimensions text
           photo: 'https://via.placeholder.com/300' + photos[i].url.slice(23) + '?text=+'
         });
       });
